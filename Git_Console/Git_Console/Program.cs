da3d18ff7c47bcb4779076aeeee19f42945d5c73
NastjaI <anastasija.isajeva@gmail.com>
2015-11-16 14:58:21 +0200

    First commit

---
+++ Git_Console/Git_Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Git_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n Isajeva Anastasija IT 3.k");
+            Console.ReadKey();
+        }
+    }
+}
